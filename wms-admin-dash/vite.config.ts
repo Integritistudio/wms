790569c612d41959297e8ebe8c8396c5ea148c31
Sajid Masood <ahmadshaukat328@gmail.com>
2026-09-19 00:16:30 +0500

    domain allowed

--- wms-admin-dash/vite.config.ts
+++ wms-admin-dash/vite.config.ts
@@ -11,7 +11,7 @@ const config = defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['wms.integritistudio.us'],
+    allowedHosts: ['wms.integritistudio.us, wms-demo.integritistudio.us'],
   },
   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
 })
